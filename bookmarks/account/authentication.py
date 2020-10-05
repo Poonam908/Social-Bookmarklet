8d874135753858e555682c60e82264d3d52ca96e
Poonam Kumari <poonambhgt74@gmail.com>
2020-10-05 12:35:06 +0530

    Django project

---
+++ bookmarks/account/authentication.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+class EmailAuthBachend(object):
+    def autheticate(self,request,username=None,password=None):
+        try:
+            user=User.objects.get(email=username)
+            if user.check_password(password):
+                return User
+        except User.DoesNotExist:
+            return None
+    def get_user(self,user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None                    
